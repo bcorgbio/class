6de7139f15671c53a2f7d489c1770fa4b84dd563
Christopher Kenaley <cpkenaley@gmail.com>
2024-09-11 08:32:00 -0400

    ZyVerNj5NP

--- sharks.R
+++ sharks.R
@@ -18,6 +18,10 @@ library(abind)#install
 library(ape)
 library(nlme)
 library(worms)
+library(Momocs)
+library(ggthemes)
+library(cowplot)
+library(OUwie)
 
 ### sharks!!
 
@@ -59,8 +63,9 @@ for(i in sp) {
   message('\r', i, appendLF = FALSE)
 }
 
-saveRDS(gbif_res,"data/shark_gbif.RDS")
+#saveRDS(gbif_res,"data/shark_gbif.RDS")
 
+gbif_res <- readRDS("data/shark_gbif.RDS")
 #no more than 10 species
 shark_n <- tibble(sp=names(gbif_res),
                   n=lapply(gbif_res, function(x) x$meta$count) %>% unlist()
@@ -77,26 +82,33 @@ fb_no <- species(shark_n$sp)$SpecCode
 
 
 fb_ln_l <- list()
-
+last_i=1
 for(i in fb_no[last_i:length(fb_no)]){
-
-  fb_i <-  fb_img_data(i)
-
   
- fb_ln_l[[i]] <- fb_i
+  fb_i <-  fb_img_data(i)
+  
+  
+  fb_ln_l[[i]] <- fb_i
   last_i <- which(i==fb_no)
+  message('\r', round(last_i/length(fb_no),2), appendLF = FALSE)
 }
+
+
 
 shark_fb_res <- do.call(rbind,fb_ln_l) %>% 
   filter(!duplicated(file_name))
 all(fb_no%in%shark_fb_res$id)
 
+shark_fb_res
+
+saveRDS(shark_fb_res,"data/shark_fb_res.RDS")
 
 
 #shark taxonomy
 
 so <- rbind(tibble(superorder="Galeomorphii",order=c("Carcharhiniformes","Heterodontiformes","Orectolobiformes","Lamniformes")),
-tibble(superorder="Squalomorphii",order=c("Hexanchiformes","Pristiophoriformes","Squaliformes","Squatiniformes","Echinorhiniformes")))
+            tibble(superorder="Squalomorphii",order=c("Hexanchiformes","Pristiophoriformes","Squaliformes","Squatiniformes","Echinorhiniformes")))
+
 
 
 tax <- wormsbynames((shark_fb_res$sp %>% unique)) %>% 
@@ -111,7 +123,7 @@ tax <- wormsbynames((shark_fb_res$sp %>% unique)) %>%
 
 shark_fb_res %>% filter(sp=="Cephaloscyllium variegatum")
 
-f <- list.files("data/shark_points",full.names = T)
+f <- list.files("data/shark_points",full.names = T,pattern=".csv")
 ldk <- read_csv(f,id="file") %>% select(file,X,Y)
 
 ldk_l <- ldk %>% 
@@ -125,6 +137,7 @@ ldk_l <- lapply(ldk_l,as.data.frame)
 ldk <-  abind(ldk_l, along=3)
 
 names(ldk) <-gsub("*_.csv","",basename(f))
+
 
 
 
@@ -167,38 +180,60 @@ PCA %>%
 
 library(ggbeeswarm)
 PCA %>% 
-  ggplot(aes(habitat,Comp1,col=superorder))+geom_beeswarm()
+  ggplot(aes(superorder,Comp1,col=habitat))+geom_beeswarm()
 
 PCA %>% 
   ggplot(aes(habitat,Comp2))+geom_beeswarm()
 
+PCA %>% 
+  ggplot(aes(habitat,size))+geom_beeswarm()
+
 phy2 <- keep.tip(phy,PCA$sp)
 X=PCA[,1:2]
 rownames(X) <- PCA$sp
-phylomorphospace(phy2[[1]],X,cex=0.2)
+phylomorphospace(phy2[[1]],X,cex=0.1)
 
 dimnames(ldk_al$coords)[[3]] <- PCA$sp
 
-gdf <- geomorph.data.frame(ldk_al,
-                           species = PCA$sp,
-                            habitat=PCA$habitat,
-                           depth=PCA$depth_max,
-                           length=PCA$size,
-                           phy=phy2[[1]]
-                            )
+fit_l <- list()
+for(i in 1:length(phy2)){
+  gdf <- geomorph.data.frame(ldk_al,
+                             species = PCA$sp,
+                             habitat=PCA$habitat,
+                             depth=log(PCA$depth_max),
+                             length=PCA$size,
+                             phy=phy2[[i]]
+  )
+  
+  fit_l[[i]] <- procD.pgls(coords ~ length, 
+                           data = gdf, phy=phy,iter = 999, turbo = TRUE,
+                           print.progress = FALSE)
+}
 
-fit1 <- procD.pgls(coords ~ habitat*depth, 
-                 data = gdf, phy=phy,iter = 999, turbo = TRUE,
-               print.progress = FALSE)
+fit_l[[1]]$pgls.residuals
+
 pc.plot <- plot(fit1, type = "PC", pch = 19)
 
 pc.plot$PC.points
 anova(fit1)
+
+## wraps
+max_PC1 <- which.max(PCA$Comp1)
+min_PC1 <- which.min(PCA$Comp1)
 M <- ldk_al$consensus
-plotRefToTarget(M, fit1$GM$fitted[,,1], mag = 1)
-plotRefToTarget(M, fit1$GM$fitted[,,20], mag = 1)
+max_PC1_warp <- plotRefToTarget(M, ldk_al$coords[,,max_PC1], mag = 1)
+plotRefToTarget(M, ldk_al$coords[,,min_PC1], mag = 1)
 
+PCA$sp[max_PC1]
+PCA$sp[min_PC1]
 
+tps_raw(M,ldk_al$coords[,,max_PC1]) %>% 
+  
+  gg_tps(fr=M,to=ldk_al$coords[,,max_PC1],geom = "path",grid = T,linewidth=0.5,alpha=0.5,amp = 0.1)+geom_polygon(data=max_PC1_xy_,aes(X1,X2+0.25),fill="grey",alpha=0.3)+scale_y_reverse()
+gg_tps(fr=M,to=ldk_al$coords[,,min_PC1],geom = "path",grid = T,linewidth=0.5,alpha=0.5,amp=0.01)+scale_y_reverse()
+max_PC1_xy <- read_tsv("data/sharks/XY_Cesil.txt")
+sc=coo_length(as.matrix(ldk_al$coords[,,max_PC1]))/coo_length(as.matrix(max_PC1_xy))
+max_PC1_xy_ <- coo_center(as.matrix(max_PC1_xy)) %>%  coo_scale(scale = 1/sc)  %>%  data.frame() 
 
 #evo rates
 
@@ -206,7 +241,7 @@ gp.so <- as.numeric(as.factor(PCA$superorder))-1
 names(gp.so) <- PCA$sp
 
 so_names <- c(PCA$superorder[which(gp.so==0)] %>% unique,
-PCA$superorder[which(gp.so==1)]%>% unique)
+              PCA$superorder[which(gp.so==1)]%>% unique)
 
 PCA$habit[which(gp.hab==0)]
 PCA$habitat[which(gp.hab==1)]
@@ -223,7 +258,7 @@ so_er_fit <- list()
 
 for(i in 1:length(phy2)){
   so_er_fit[[i]] <-compare.evol.rates(A=ldk_al$coords, phy=phy2[[i]],
-                       method="simulation",gp=gp.so,iter=999,print.progress = F)
+                                      method="simulation",gp=gp.so,iter=999,print.progress = F)
 }
 
 so_er <- lapply(so_er_fit,summary)
@@ -244,7 +279,7 @@ hab_er_fit <- list()
 
 for(i in 1:length(phy2)){
   hab_er_fit[[i]] <-compare.evol.rates(A=ldk_al$coords, phy=phy2[[i]],
-                                      method="simulation",gp=gp.hab,iter=999,print.progress = F)
+                                       method="simulation",gp=gp.hab,iter=999,print.progress = F)
 }
 
 hab_er <- lapply(hab_er_fit,summary)
@@ -261,13 +296,109 @@ hab_er %>%
   ggplot(aes(value,fill=name)) + geom_histogram()
 
 
-ER<-compare.evol.rates(A=ldk_al$coords, phy=phy2[[1]],
-                       method="simulation",gp=gp.hab,iter=999)
-summary(ER)
-plot(ER)
+### distances
+dist_df <- data.frame(
+  start=c(1,1,1,12,1,1),
+  end=c(2,12,10,10,5,8),
+  var=c("predorsal","prepectoral","prepelvic","interpaired","precaudal_1","precaudal_2")
+  
+)
+dist_ldk <- geomorph::interlmkdist(ldk_al$coords,lmks = dist_df[,1:2]) %>% data.frame()
+
+colnames(dist_ldk) <- dist_df$var
+dist_ldk <- dist_ldk %>% 
+  mutate(sp=rownames(dist_ldk)) %>% 
+  left_join(hab %>% select(sp,habitat,depth)) %>% 
+  mutate(predorsal=predorsal/(mean(c(precaudal_1,precaudal_2))),
+         prepectoral=prepectoral/(mean(c(precaudal_1,precaudal_2))),
+         prepelvic=prepelvic/(mean(c(precaudal_1,precaudal_2))),
+         interpaired=interpaired/(mean(c(precaudal_1,precaudal_2)))
+  )
+dist_ldk_long <- dist_ldk %>% 
+  pivot_longer(predorsal:interpaired)
+dist_ldk_long %>% 
+  ggplot(aes(habitat,value))+geom_beeswarm()+geom_boxplot(alpha=0.3)+facet_wrap(.~name)
+
+fit_dist <- lm(value~habitat,data=dist_ldk_long %>% filter(name=="interpaired"))
+anova(fit_dist)
+
+fit_dist <- lm(value~habitat,data=dist_ldk_long %>% filter(name=="predorsal"))
+anova(fit_dist)
+
+fit_dist <- lm(value~habitat,data=dist_ldk_long %>% filter(name=="prepectoral"))
+anova(fit_dist)
+
+fit_dist <- lm(value~habitat,data=dist_ldk_long %>% filter(name=="prepelvic"))
+anova(fit_dist)
+
+
+##houwei
+args_df <- expand_grid(discrete_model=c("ER","ARD"),
+                       continuous_model=c("BM1", "BMV", "OU1", "OUM", "OUA", "OUV", "OUMV", "OUMA", "OUVA", "OUMVA"),
+                       var=unique(dist_ldk_long$name)) %>% 
+  mutate(n=1:n(),
+         nSim=25,
+         n_starts=5,
+         rate.cat=1) %>% 
+  arrange(var)
+
+
+   
+
+args <- args_df %>% 
+  group_by(n) %>% 
+  group_split(.keep=F)
+
+data_l <- dist_ldk_long %>% 
+  select(sp,habitat,value,name) %>% 
+  group_by(name) %>% 
+  group_split(.keep=F)
+
+args<-lapply(args, FUN=append,
+          list(phy=phy2[[1]]),
+          )
+
+for(i in 1:length(args)){
+  args[[i]]$data <- dist_ldk_long %>% filter(name==args[[i]]$var) %>% select(sp,habitat,value) %>% data.frame()
+  args[[i]]$var <- NULL
+}
+               
+run_hOUwie <- function(x) do.call(hOUwie,args[[x]])
+
+hOUwie_res<- mclapply(X=1:length(args),FUN = run_hOUwie,mc.cores = detectCores()-2)
+
+hOUwie_res_eval <- getModelTable(hOUwie_res) %>% tibble() %>% 
+  tibble(args_df %>% select(var,discrete_model,continuous_model))
+
+hOUwie_res_eval %>% 
+  group_by(var) %>% 
+  filter(BIC==min(BIC))
+
+
+sapply(X=q, `[[`, "root.p")
+sapply(X=q, `[[`, "node.states")
+sapply(X=q, `[[`, "get.tip.states")
+hOUwie_res_inter <- mclapply(X=1:15,FUN = run_hOUwie,mc.cores = detectCores()-2)
+hOUwie_res_preD <- mclapply(X=16:30,FUN = run_hOUwie,mc.cores = detectCores()-2)
+hOUwie_res_preP <- mclapply(X=31:45,FUN = run_hOUwie,mc.cores = detectCores()-2)
+
+hOUwie_res_inter_eval <- getModelTable(hOUwie_res_inter)
+hOUwie_res_preD_eval <- getModelTable(hOUwie_res_preD)
+hOUwie_res_preP_eval <- getModelTable(hOUwie_res_preP)
+
+args[1:15][which.min(hOUwie_res_inter_eval$BIC)]
+args[16:30][which.min(hOUwie_res_preD_eval$BIC)]
+args[31:45][which.min(hOUwie_res_preP_eval$BIC)]
+
+hOUwie_res_preD[[which.min(hOUwie_res_preD_eval$BIC)]]
+hOUwie_res_preP[[which.min(hOUwie_res_preP_eval$BIC)]]
+trait_dat <- dist_ldk %>% select(sp,habitat,predorsal)
+pp_oum <- hOUwie(phy2[[1]], trait_dat, rate.cat = 1, discrete_model = "ER", 
+                 continuous_model = "OUM", nSim = 25,ncores = detectCores()-2,n_starts = 5)
+
 
 fb_img_data <- function(x,type=c("museum","collaborator")){
-
+  
   url<- paste0("https://fishbase.mnhn.fr/photos/ThumbnailsSummary.php?ID=",x)
   
   sp <- fb_tbl(tbl="species",server="fishbase") %>% filter(SpecCode==x) %>% select(Genus,Species)
@@ -276,39 +407,162 @@ fb_img_data <- function(x,type=c("museum","collaborator")){
   url <-  url(url, "rb")
   try_e <- T
   
-    
-    url_ <-  url %>%  read_html()
-    
-    
   
-    links <-url_ %>% 
-      html_nodes("img") %>%
-      html_attr("src") %>% 
-      unlist
-    
-    close(url)
-    
-    links <- links[!grepl("thumbnails|Upload|mediaphoto",links)]
-    if(length(links)>0){
+  url_ <-  url %>%  read_html()
+  
+  
+  
+  links <-url_ %>% 
+    html_nodes("img") %>%
+    html_attr("src") %>% 
+    unlist
+  
+  close(url)
+  
+  links <- links[!grepl("thumbnails|Upload|mediaphoto",links)]
+  if(length(links)>0){
     link_f <- paste0("https://fishbase.mnhn.fr/images/species/",basename(links[grepl(".jpg|.gif",links)]))
- 
- link_f <- link_f[grepl("species",link_f)]
- link_f <- link_f[!grepl("&w",link_f)]
- 
- 
- if(!dir.exists(paste0("data/shark_photos/",sp))) dir.create(paste0("data/shark_photos/",sp))
- dest_f <- paste0("data/shark_photos/",sp,"/",basename(link_f))
- 
- res_l <- lapply(1:length(link_f),function(x) try(curl::curl_download(link_f[x],dest_f[x])) )  
- 
- res_err <- lapply(res_l,class) %>% unlist
- 
- res_f <- res_l[!grepl('try-error',res_err)] %>% unlist %>% basename
- res <- tibble(sp=sp,id=x,file_name=res_f)
-    }else{
-      res <- tibble(sp=sp,id=x,file_name=NA)
-    }
-
-
+    
+    link_f <- link_f[grepl("species",link_f)]
+    link_f <- link_f[!grepl("&w",link_f)]
+    
+    
+    if(!dir.exists(paste0("data/shark_photos/",sp))) dir.create(paste0("data/shark_photos/",sp))
+    dest_f <- paste0("data/shark_photos/",sp,"/",basename(link_f))
+    
+    res_l <- lapply(1:length(link_f),function(x) try(curl::curl_download(link_f[x],dest_f[x])) )  
+    
+    res_err <- lapply(res_l,class) %>% unlist
+    
+    res_f <- res_l[!grepl('try-error',res_err)] %>% unlist %>% basename
+    res <- tibble(sp=sp,id=x,file_name=res_f)
+  }else{
+    res <- tibble(sp=sp,id=x,file_name=NA)
+  }
+  
+  
   return(res)
+}
+
+
+### ggplotify tps plots
+gg_tps <- function (fr, to, amp = 1, over = 1.2,grid=F, grid.size = 7,
+                    geom="path",plot_shape="to",diff_line=0.5,diff_alpha=0.5,facet=F,rot=F,flip=NULL,add_opts=NULL,...) 
+{
+  args <- list(...)
+  fr.n <- substitute(fr)
+  to.n <- substitute(to)
+  if (!missing(amp)) {
+    to <- to + (to - fr) * amp
+  }
+  
+  if(!is.null(rot)){ 
+    fr <- fr  %>% coo_rotate(rot*pi/180)
+    to <- to  %>% coo_rotate(rot*pi/180)
+    colnames(fr) <- colnames(to) <- c("X","Y")
+  }
+  
+  if(!is.null(flip)){ 
+    
+    if(flip=="flip_x"){
+      fr <- fr  %>% coo_flipx()
+      to <- to  %>% coo_flipx()
+    }
+    
+    if(flip=="flip_y"){
+      fr <- fr  %>% coo_flipy()
+      to <- to  %>% coo_flipy()
+    }
+    
+    colnames(fr) <- colnames(to) <- c("X","Y")
+  }
+  
+  grid0 <- Momocs:::.grid.sample(fr, to, nside = round(grid.size), over = over)
+  grid1 <- tps2d(grid0, fr, to)
+  dim.grid <- c(length(unique(grid0[, 1])), length(unique(grid0[, 
+                                                                2])))
+  
+  if(all(grepl("[.]", rownames(to)))) {
+    fr_df <-
+      fr %>% coo_close %>% data.frame %>% mutate(grp = gsub("(.*)[.].*", "\\1", rownames(fr)))
+    to_df <-
+      to %>% oo_close %>% data.frame %>% mutate(grp = gsub("(.*)[.].*", "\\1", rownames(to)))
+    
+    grp <- gsub("(.*)[.].*", "\\1", rownames(fr)) %>% unique
+    
+    
+    if (geom == "path") {
+      to_p <-  geom_polygon(data = to_df, aes(X, Y, col = grp), ...)
+      fr_p <-  geom_polygon(data = fr_df, aes(X, Y, col = grp), ...)
+    }
+    if (geom == "point") {
+      to_p <-  geom_point(data = to_df, aes(X, Y, col = grp), ...)
+      fr_p <-  geom_point(data = fr_df, aes(X, Y, col = grp), ...)
+    }
+    
+    
+  }else{
+    
+    fr_df <- fr %>% data.frame
+    to_df <- to %>% data.frame
+    
+    
+    
+    
+    if(geom=="path") {
+      to_p <-  geom_polygon(data=to_df,aes(X,Y),...) 
+      fr_p <-  geom_polygon(data=fr_df,aes(X,Y),...)
+    }
+    if(geom=="point") {to_p <-  geom_point(data=to_df,aes(X,Y),...)
+    fr_p <-  geom_point(data=fr_df,aes(X,Y),...)
+    }
+  }
+  
+  if(plot_shape=="to") p_shape <- to_p
+  if(plot_shape=="from") p_shape <- fr_p
+  
+  
+  if(plot_shape=="both"){
+    both_df <- rbind(
+      fr_df %>% mutate(shape="from"),
+      to_df %>% mutate(shape="to")
+    )
+    if(geom=="point") p_shape <- geom_point(data=both_df,aes(X,Y,col=shape),...)
+    if(geom=="path") p_shape <- geom_path(data=both_df,aes(X,Y,col=shape),...)
+  }
+  
+  
+  p <- ggplot()
+  
+  
+  seg_1 <- list()
+  for (i in 1:dim.grid[2]) {
+    xy_i <- grid1[(1:dim.grid[1]) + (i - 1) * dim.grid[1], ] %>% data.frame()
+    seg_1[[i]] <- geom_path(data=xy_i,aes(X1,X2),...)
+    seg_1[[i]]$aes_params$alpha <-  p_shape$aes_params$alpha*diff_alpha
+    seg_1[[i]]$aes_params$linewidth <-  p_shape$aes_params$linewidth*diff_line
+  }
+  
+  seg_2 <- list()
+  for (i in 1:dim.grid[1]) {
+    xy_i <- grid1[(1:dim.grid[2]) * dim.grid[1] - i + 1, ] %>% data.frame
+    seg_2[[i]] <- geom_path(data=xy_i,aes(X1,X2),...)
+    seg_2[[i]]$aes_params$alpha <-  p_shape$aes_params$alpha*diff_alpha
+    seg_2[[i]]$aes_params$linewidth <-  p_shape$aes_params$linewidth*diff_line
+  }
+  
+  if(grid) p <- p+seg_1+seg_2+p_shape+theme_nothing()+coord_equal()
+  if(!grid) p <- p+p_shape+theme_nothing()+coord_equal()
+  
+  if(facet==T) {
+    p <- p+facet_wrap(.~grp,nrow=1)
+  }
+  
+  
+  
+  if(!is.null(add_opts)) {
+    p <- p+add_opts
+  }
+  
+  return(p)
 }
