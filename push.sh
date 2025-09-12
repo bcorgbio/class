168737c4109349fc20a2a1b982ff7d468d12573a
Christopher Kenaley <cpkenaley@gmail.com>
2025-09-12 15:53:48 -0400

    mp2 features

---
+++ push.sh
@@ -0,0 +1,4 @@
+cd /Users/Chris/Dropbox/Documents/bc\ \(1\)/3140.f25/ignore/personal_repos
+git clone https://github.com/Brogan-D/doughebj.git
+cd doughebj
+git pull https://github.com/Brogan-D/doughebj.git
